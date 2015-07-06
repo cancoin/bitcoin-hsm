2f56e4ce17014e9b47fc3319226c2130bfa3049c
ty <lastcanal@gmail.com>
2015-07-06 13:28:40 -0400

    fix mix port compiling and cleaning

--- lib/mix/tasks/clean/cport.ex
+++
@@ -1,9 +0,0 @@
-defmodule Mix.Tasks.Clean.Cport do
-  use Mix.Task
-  import Mix.Generator
-
-  def run(_) do
-    {message, 0} = System.cmd("make", ["-C", "./c_src", "clean"], [])
-    message |> String.split("\n") |> Enum.map &IO.inspect(&1)
-  end
-end
