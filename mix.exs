16a6e237983199be998cfefc7e4a0c8fb1daaa92
ty <lastcanal@gmail.com>
2015-09-09 15:41:45 -0400

    remove c port compiler from mix task

--- mix.exs
+++ mix.exs
@@ -1,24 +1,3 @@
-
-defmodule Mix.Tasks.Compile.Cport do
-  use Mix.Task
-
-  @shortdoc "compile c-port in c_src"
-  def run(_args) do
-    if Mix.shell.cmd("make -C c_src") != 0 do
-      raise Mix.Error,
-        message: "Failed to compile C-Port"
-    end
-  end
-
-  def clean do
-    if Mix.shell.cmd("make -C c_src clean") != 0 do
-      raise Mix.Error,
-        message: "Failed to clean C-Port"
-    end
-
-  end
-end
-
 defmodule BitcoinHSM.Mixfile do
   use Mix.Project
 
